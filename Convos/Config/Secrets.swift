87a1e4fb5a377521f14baad83eaa0f31ceb30e7f
Joe Ferrucci <ahoy@joeferrucci.info>
2025-04-08 16:23:34 -0500

    Add Secrets.swift (will remove it from git tracking in next commit)

---
+++ Convos/Config/Secrets.swift
@@ -0,0 +1,20 @@
+//
+//  Secrets.swift
+//  Convos
+//
+//  Created by Joe Ferrucci on 4/8/25.
+//
+
+import Foundation
+
+// DO NOT commit Secrets.swift to version control.
+
+enum Secrets {
+    // Example API keys and secrets
+    static let apiKey: String = "YOUR_API_KEY_HERE"
+    static let clientSecret: String = "YOUR_CLIENT_SECRET_HERE"
+
+    // Add more secrets as needed
+    // static let anotherSecret: String = "YOUR_ANOTHER_SECRET_HERE"
+}
+
