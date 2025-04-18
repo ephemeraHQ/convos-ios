42e2d2337f7af814a2f6a85290d54cc5544c20c1
Jarod Luebbert <jarod@ephemerahq.com>
2025-04-18 08:51:05 -0700

    Add PostHog for Analytics

--- Convos/Convos/Landing/RootViewController.swift
+++ Convos/Convos/Landing/RootViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 
 final class RootViewController: UIViewController {
     let authService = AuthService()
+    let analyticsService = PosthogAnalyticsService.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let rootView = RootView(authService: authService)
+        
+        let rootView = RootView(authService: authService,
+                                analyticsService: analyticsService)
         let hostingController = UIHostingController(rootView: rootView)
         addChild(hostingController)
         view.addSubview(hostingController.view)
