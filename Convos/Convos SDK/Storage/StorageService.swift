04bccc3b4e8bad61e64b2575fc74d18cc01cde0f
Jarod Luebbert <jarod@ephemerahq.com>
2025-04-25 10:40:30 -0700

    delete dead code

--- Convos/Convos SDK/Storage/StorageService.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public extension ConvosSDK {
-    protocol StorageServiceProtocol {
-        func save<T: Encodable>(_ value: T, for key: String) throws
-        func retrieve<T: Decodable>(_ key: String) throws -> T?
-        func delete(_ key: String) throws
-    }
-}
