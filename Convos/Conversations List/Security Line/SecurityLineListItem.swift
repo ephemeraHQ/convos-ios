0f721a6e48d8b16c31620906d5effc5b928b213c
Jarod Luebbert <jarodluebbert@gmail.com>
2025-06-07 13:07:48 -0700

    simplify security line emblem view

--- Convos/Conversations List/Security Line/SecurityLineListItem.swift
+++ Convos/Conversations List/Security Line/SecurityLineListItem.swift
@@ -2,22 +2,22 @@ import SwiftUI
 
 struct SecurityLineEmblemView: View {
     var body: some View {
-        GeometryReader { geometry in
-            HStack {
-                Image(systemName: "shield.fill")
-                    .font(.system(size: 24.0))
-                    .multilineTextAlignment(.center)
-                    .padding(.vertical, 9.0)
-                    .padding(.horizontal, 15.0)
-                    .foregroundStyle(.colorTextPrimaryInverted)
-                    .aspectRatio(1.0, contentMode: .fill)
-            }
-            .frame(width: geometry.size.height, height: geometry.size.height)
-            .background(.colorOrange)
-            .clipShape(RoundedRectangle(cornerRadius: DesignConstants.CornerRadius.medium))
+        HStack {
+            Image(systemName: "shield.fill")
+                .font(.system(size: 24.0))
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 9.0)
+                .padding(.horizontal, 15.0)
+                .foregroundStyle(.colorTextPrimaryInverted)
+                .aspectRatio(1.0, contentMode: .fit)
         }
+        .aspectRatio(1.0, contentMode: .fit)
+        .frame(maxHeight: .infinity)
+        .background(.colorOrange)
+        .clipShape(RoundedRectangle(cornerRadius: DesignConstants.CornerRadius.medium))
     }
 }
+
 struct SecurityLineListItem: View {
     let count: Int
     var body: some View {
