ecf41d9029a23fadeeda9354d07647b2b1e12f56
Jarod Luebbert <jarodluebbert@gmail.com>
2025-06-07 20:41:44 -0700

    fix bottom bar in messages nav

--- Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Views/MessagesNavigationBar.swift
+++ Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Views/MessagesNavigationBar.swift
@@ -4,9 +4,6 @@ import UIKit
 class MessagesNavigationBar: UIView {
     // MARK: - Properties
 
-    private let blurView: UIVisualEffectView = UIVisualEffectView(
-        effect: UIBlurEffect(style: .regular)
-    )
     private(set) var viewModel: MessagesToolbarViewModel = MessagesToolbarViewModel()
     private var hostingController: UIHostingController<MessagesToolbarView>?
 
@@ -34,24 +31,6 @@ class MessagesNavigationBar: UIView {
     // MARK: - Private Methods
 
     private func setupUI() {
-        backgroundColor = .white.withAlphaComponent(0.8)
-        blurView.layer.borderWidth = 0
-        blurView.layer.backgroundColor = UIColor.clear.cgColor
-        blurView.layer.masksToBounds = false
-        blurView.layer.shadowColor = UIColor.systemGray5.cgColor
-        blurView.layer.shadowOffset = CGSize(width: 0, height: 1)
-        blurView.layer.shadowOpacity = 1
-        blurView.layer.shadowRadius = 0
-
-        addSubview(blurView)
-        blurView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            blurView.topAnchor.constraint(equalTo: topAnchor),
-            blurView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            blurView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            blurView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
-
         let swiftUIView = MessagesToolbarView(viewModel: viewModel)
         let hostingController = UIHostingController(rootView: swiftUIView)
         hostingController.view.backgroundColor = .clear
