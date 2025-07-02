bf14d0abfac942b6b873a22a36ca3b73351a45c1
Jarod Luebbert <jarodluebbert@gmail.com>
2025-07-01 18:52:51 -0700

    setup script fix for bitrise

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -55,16 +55,11 @@ if [ ! "${CI}" = true ]; then
   defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool true
   defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 120
   defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool true
+fi
 
   # Skip fingerprint validation for plugins and macros in Xcode (like SwiftLintBuildToolPlugin)
   defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
   defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
-else
-  info "CI environment detected - setting Xcode defaults for CI..."
-  # In CI, also skip plugin validation to avoid build failures
-  defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
-  defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
-fi
 
 ################################################################################
 # Setup Dependencies                                                           #
