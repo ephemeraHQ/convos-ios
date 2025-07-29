3fb31d196a3d080e8b641fc667e8a5c41e9296e1
Jarod Luebbert <jarodluebbert@gmail.com>
2025-07-29 16:32:50 -0700

    simplify invite cell layout

--- Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Cells/InviteCell.swift
+++ Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Cells/InviteCell.swift
@@ -2,10 +2,25 @@ import SwiftUI
 import UIKit
 
 class InviteCell: UICollectionViewCell {
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        contentConfiguration = nil
+    }
+
     func prepare(with invite: Invite, hasVerticalPadding: Bool) {
         contentConfiguration = UIHostingConfiguration {
-            InviteView(invite: invite, hasVerticalPadding: hasVerticalPadding)
+            InviteView(invite: invite, hasVerticalPadding: false)
         }
+        .margins(.vertical, 0.0)
+    }
+
+    override func preferredLayoutAttributesFitting(
+        _ layoutAttributes: UICollectionViewLayoutAttributes
+    ) -> UICollectionViewLayoutAttributes {
+        let targetSize = CGSize(width: layoutAttributes.size.width,
+                                height: layoutAttributes.size.width)
+        layoutAttributes.size.height = targetSize.height
+        return layoutAttributes
     }
 }
 
@@ -20,13 +35,12 @@ struct InviteView: View {
                     identifier: invite.temporaryInviteString, // invite.inviteUrlString, @jarodl temporary
                     backgroundColor: .colorFillMinimal
                 )
+                .frame(maxWidth: 220, maxHeight: 220)
                 .padding(DesignConstants.Spacing.step12x)
             }
             .background(.colorFillMinimal)
             .mask(RoundedRectangle(cornerRadius: 38.0))
-            .padding(.vertical, hasVerticalPadding ? 136.0 : 0.0)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
