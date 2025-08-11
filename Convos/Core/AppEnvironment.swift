e477e0abd7c12b0550f8df2655d4b36d10ffb3b4
Louis Rouffineau <dev@louis.io>
2025-08-11 12:27:06 +0200

    Add shouldShowDebugErrors

--- Convos/Core/AppEnvironment.swift
+++ Convos/Core/AppEnvironment.swift
@@ -108,4 +108,14 @@ enum AppEnvironment {
         }
         return groupUrl
     }
+
+    /// Whether debug error overlays should be shown in this environment
+    var shouldShowDebugErrors: Bool {
+        switch self {
+        case .local, .dev:
+            return true
+        case .tests, .production:
+            return false
+        }
+    }
 }
