f167047d10456b0cf1ec12050e24b6c781ee2cc6
Jarod Luebbert <jarodluebbert@gmail.com>
2025-08-14 16:03:27 -0700

    remove sentry and posthog

--- Convos/ConvosApp.swift
+++ Convos/ConvosApp.swift
@@ -3,13 +3,11 @@ import SwiftUI
 @main
 struct ConvosApp: App {
     let convos: ConvosClient = .client(environment: ConfigManager.shared.currentEnvironment)
-    let analyticsService: AnalyticsServiceProtocol = PosthogAnalyticsService.shared
 
     @UIApplicationDelegateAdaptor(PushNotificationDelegate.self) var pushDelegate: PushNotificationDelegate
     @State private var pushNotificationManager: PushNotificationManager = PushNotificationManager.shared
 
     init() {
-        SDKConfiguration.configureSDKs()
         Logger.info("🚀 App starting with environment: \(ConfigManager.shared.currentEnvironment)")
 
         do {
