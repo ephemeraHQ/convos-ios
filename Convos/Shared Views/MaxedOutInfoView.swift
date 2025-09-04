832d9dbd6b0601ba68d9c9b509875624104982d2
Jarod Luebbert <jarodluebbert@gmail.com>
2025-09-04 10:58:03 -0700

    show maxed out convos info

---
+++ Convos/Shared Views/MaxedOutInfoView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct MaxedOutInfoView: View {
+    let maxNumberOfConvos: Int
+
+    @Environment(\.dismiss) var dismiss: DismissAction
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: DesignConstants.Spacing.step4x) {
+            Text("Maxed out")
+                .font(.system(.largeTitle))
+                .fontWeight(.bold)
+            Text("This early version of the app supports up to \(maxNumberOfConvos) convos. Consider exploding some to make room for new ones.")
+                .font(.body)
+                .foregroundStyle(.colorTextSecondary)
+
+            VStack(spacing: DesignConstants.Spacing.step2x) {
+                Button {
+                    dismiss()
+                } label: {
+                    Text("Got it")
+                }
+                .convosButtonStyle(.rounded(fullWidth: true))
+            }
+            .padding(.top, DesignConstants.Spacing.step4x)
+        }
+        .padding([.leading, .top, .trailing], DesignConstants.Spacing.step10x)
+    }
+}
+
+#Preview {
+    MaxedOutInfoView(maxNumberOfConvos: 20)
+}
