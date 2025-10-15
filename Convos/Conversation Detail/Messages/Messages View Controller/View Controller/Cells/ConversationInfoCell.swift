811db3c56e3f0c609ed6ac2d6c1841c4708b88de
Jarod Luebbert <jarodluebbert@gmail.com>
2025-10-15 13:45:03 -0700

    add earlier messages are hidden text

--- Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Cells/ConversationInfoCell.swift
+++ Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Cells/ConversationInfoCell.swift
@@ -41,47 +41,47 @@ struct ConversationInfoPreview: View {
     let conversation: Conversation
 
     var body: some View {
-        VStack {
-            VStack(spacing: DesignConstants.Spacing.step2x) {
-                ConversationAvatarView(
-                    conversation: conversation,
-                    conversationImage: nil
-                )
-                .frame(width: 96.0, height: 96.0)
+        VStack(spacing: DesignConstants.Spacing.step4x) {
+            VStack {
+                VStack(spacing: DesignConstants.Spacing.step2x) {
+                    ConversationAvatarView(
+                        conversation: conversation,
+                        conversationImage: nil
+                    )
+                    .frame(width: 96.0, height: 96.0)
 
-                VStack(spacing: DesignConstants.Spacing.stepHalf) {
-                    Text(conversation.displayName)
-                    .font(.headline.weight(.semibold))
-                    .foregroundStyle(.colorTextPrimary)
-                    if let description = conversation.description, !description.isEmpty {
-                        Text(description)
-                            .font(.subheadline.weight(.regular))
+                    VStack(spacing: DesignConstants.Spacing.stepHalf) {
+                        Text(conversation.displayName)
+                            .font(.headline.weight(.semibold))
                             .foregroundStyle(.colorTextPrimary)
+                        if let description = conversation.description, !description.isEmpty {
+                            Text(description)
+                                .font(.subheadline.weight(.regular))
+                                .foregroundStyle(.colorTextPrimary)
+                        }
                     }
+                    .padding(.horizontal, DesignConstants.Spacing.step2x)
+
+                    Text(conversation.membersCountString)
+                        .font(.caption)
+                        .foregroundStyle(.colorTextSecondary)
                 }
-                .padding(.horizontal, DesignConstants.Spacing.step2x)
-
-                Text(conversation.membersCountString)
-                    .font(.caption)
-                    .foregroundStyle(.colorTextSecondary)
+                .multilineTextAlignment(.center)
+                .padding(DesignConstants.Spacing.step6x)
             }
-            .multilineTextAlignment(.center)
-            .padding(DesignConstants.Spacing.step6x)
+            .frame(maxWidth: .infinity)
+            .background(.colorFillMinimal)
+            .clipShape(RoundedRectangle(cornerRadius: DesignConstants.CornerRadius.mediumLarger))
 
-//            if true {
-//                Rectangle()
-//                    .fill(.colorBorderSubtle)
-//                    .frame(height: 1.0)
-//                    .frame(maxWidth: .infinity)
-//                VStack {
-//
-//                }
-//                .padding(DesignConstants.Spacing.step6x)
-//            }
+            HStack(spacing: DesignConstants.Spacing.stepX) {
+                Image(systemName: "clock.fill")
+
+                Text("Earlier messages are hidden for privacy")
+                    .foregroundStyle(.colorTextSecondary)
+                    .padding(.vertical, DesignConstants.Spacing.step2x)
+            }
+            .font(.caption)
         }
-        .frame(maxWidth: .infinity)
-        .background(.colorFillMinimal)
-        .clipShape(RoundedRectangle(cornerRadius: DesignConstants.CornerRadius.mediumLarger))
     }
 }
 
