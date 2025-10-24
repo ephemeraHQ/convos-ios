d1a9812f79143dfd8aeff8aaa341631f89b88e5d
Louis Rouffineau <dev@louis.io>
2025-10-23 19:48:07 -0700

    Scheme-specific Info.plist files

--- Convos/Config/InfoPlistPrefix.Dev.h
+++
@@ -1,6 +0,0 @@
-// Info.plist preprocessing definitions for Dev configuration
-
-// ATS_DICTIONARY is not defined for Dev, so NSAppTransportSecurity won't be added
-#define ATS_DICTIONARY
-
-#define LOCAL_NETWORK_USAGE
--- Convos/Config/InfoPlistPrefix.Prod.h
+++
@@ -1,6 +0,0 @@
-// Info.plist preprocessing definitions for Prod configuration
-
-// ATS_DICTIONARY is not defined for Prod, so NSAppTransportSecurity won't be added
-#define ATS_DICTIONARY
-
-#define LOCAL_NETWORK_USAGE
