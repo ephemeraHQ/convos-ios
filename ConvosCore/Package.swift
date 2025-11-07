3249b401576dc30cfcab67f53a4c45b3706215bd
lourou <lourou@users.noreply.github.com>
2025-11-07 17:12:48 +0100

    Align SwiftLint to the version CI is running: 0.62.2 (#225)

--- ConvosCore/Package.swift
+++ ConvosCore/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/groue/GRDB.swift.git", exact: "7.5.0"),
         .package(url: "https://github.com/xmtp/xmtp-ios.git", exact: "4.6.1-dev.c11139e"),
         .package(url: "https://github.com/tesseract-one/CSecp256k1.swift.git", from: "0.2.0"),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.61.0"),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.62.2"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.1.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.1"),
         .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.57.1"),
