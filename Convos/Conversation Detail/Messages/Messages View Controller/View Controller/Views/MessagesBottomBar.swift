f41be2ea90c5d9acae2b6271b93f657a18c3c091
Jarod Luebbert <jarodluebbert@gmail.com>
2025-11-07 11:27:44 -0800

    Fix issues with quick edit component width

--- Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Views/MessagesBottomBar.swift
+++ Convos/Conversation Detail/Messages/Messages View Controller/View Controller/Views/MessagesBottomBar.swift
@@ -22,95 +22,58 @@ struct MessagesBottomBar: View {
     let onProfileSettings: () -> Void
 
     @State private var progress: CGFloat = 0.0
-    //    @State private var isExpanded: Bool = false
+    @State private var isExpanded: Bool = false
     @Namespace private var namespace: Namespace.ID
 
     var body: some View {
-        PrimarySecondaryContainerView(
-            progress: progress,
-            primaryProperties: .init(
-                cornerRadius: (MessagesInputView.defaultHeight + (DesignConstants.Spacing.step4x)) / 2.0,
-                padding: DesignConstants.Spacing.step2x,
-                fixedSizeHorizontal: false
-            ),
-            secondaryProperties: .init(
-                cornerRadius: 40.0,
-                padding: DesignConstants.Spacing.step6x,
-                fixedSizeHorizontal: false
-            )
-        ) {
-            MessagesInputView(
-                profile: profile,
-                profileImage: $profileImage,
-                displayName: $displayName,
-                emptyDisplayNamePlaceholder: emptyDisplayNamePlaceholder,
-                messageText: $messageText,
-                sendButtonEnabled: $sendButtonEnabled,
-                focusState: $focusState,
-                animateAvatarForQuickname: animateAvatarForQuickname,
-                messagesTextFieldEnabled: messagesTextFieldEnabled,
-                onProfilePhotoTap: onProfilePhotoTap,
-                onSendMessage: onSendMessage
-            )
-        } secondaryContent: {
-            QuickEditView(
-                placeholderText: "\(emptyDisplayNamePlaceholder)...",
-                text: $displayName,
-                image: $profileImage,
-                focusState: $focusState,
-                focused: .displayName,
-                onSubmit: onDisplayNameEndedEditing,
-                onSettings: onProfileSettings
-            )
+        GlassEffectContainer {
+            ZStack {
+                if !isExpanded {
+                    MessagesInputView(
+                        profile: profile,
+                        profileImage: $profileImage,
+                        displayName: $displayName,
+                        emptyDisplayNamePlaceholder: emptyDisplayNamePlaceholder,
+                        messageText: $messageText,
+                        sendButtonEnabled: $sendButtonEnabled,
+                        focusState: $focusState,
+                        animateAvatarForQuickname: animateAvatarForQuickname,
+                        messagesTextFieldEnabled: messagesTextFieldEnabled,
+                        onProfilePhotoTap: onProfilePhotoTap,
+                        onSendMessage: onSendMessage
+                    )
+                    .fixedSize(horizontal: false, vertical: true)
+                    .clipShape(.rect(cornerRadius: 26.0))
+                    .glassEffect(.regular.interactive(), in: .rect(cornerRadius: 26.0))
+                    .glassEffectID("input", in: namespace)
+                    .glassEffectTransition(.matchedGeometry)
+                }
+
+                if isExpanded {
+                    QuickEditView(
+                        placeholderText: "\(emptyDisplayNamePlaceholder)...",
+                        text: $displayName,
+                        image: $profileImage,
+                        focusState: $focusState,
+                        focused: .displayName,
+                        onSubmit: onDisplayNameEndedEditing,
+                        onSettings: onProfileSettings
+                    )
+                    .frame(maxWidth: 320.0)
+                    .padding(DesignConstants.Spacing.step6x)
+                    .clipShape(.rect(cornerRadius: 40.0))
+                    .glassEffect(.regular.interactive(), in: .rect(cornerRadius: 40.0))
+                    .glassEffectID("profileEditor", in: namespace)
+                    .glassEffectTransition(.matchedGeometry)
+                }
+            }
         }
-//        GlassEffectContainer {
-//            ZStack {
-//                if !isExpanded {
-//                    MessagesInputView(
-//                        profile: profile,
-//                        profileImage: $profileImage,
-//                        displayName: $displayName,
-//                        emptyDisplayNamePlaceholder: emptyDisplayNamePlaceholder,
-//                        messageText: $messageText,
-//                        sendButtonEnabled: $sendButtonEnabled,
-//                        focusState: $focusState,
-//                        onProfilePhotoTap: onProfilePhotoTap,
-//                        onSendMessage: onSendMessage
-//                    )
-//                    .clipShape(.rect(cornerRadius: 26.0))
-//                    .glassEffect(.regular.interactive(), in: .rect(cornerRadius: 26.0))
-//                    .glassEffectID("input", in: namespace)
-//                    .glassEffectTransition(.matchedGeometry)
-//                }
-//
-//                if isExpanded {
-//                    QuickEditView(
-//                        placeholderText: "\(emptyDisplayNamePlaceholder)...",
-//                        text: $displayName,
-//                        image: $profileImage,
-//                        focusState: $focusState,
-//                        focused: .displayName,
-//                        onSubmit: onDisplayNameEndedEditing,
-//                        onSettings: onProfileSettings
-//                    )
-//                    .frame(maxWidth: 320.0)
-//                    .padding(DesignConstants.Spacing.step6x)
-//                    .clipShape(.rect(cornerRadius: 40.0))
-//                    .glassEffect(.regular.interactive(), in: .rect(cornerRadius: 40.0))
-//                    .glassEffectID("profileEditor", in: namespace)
-//                    .glassEffectTransition(.matchedGeometry)
-//                }
-//            }
-//        }
         .padding(.horizontal, 10.0)
         .padding(.vertical, DesignConstants.Spacing.step2x)
         .onChange(of: viewModelFocus) { _, newValue in
-            withAnimation(.bouncy(duration: 0.5, extraBounce: 0.2)) {
-                progress = newValue == .displayName ? 1.0 : 0.0
+            withAnimation(.bouncy(duration: 0.4, extraBounce: 0.01)) {
+                isExpanded = newValue == .displayName ? true : false
             }
-//            withAnimation(.bouncy(duration: 0.4, extraBounce: 0.01)) {
-//                isExpanded = newValue == .displayName ? true : false
-//            }
         }
     }
 }
@@ -124,23 +87,48 @@ struct MessagesBottomBar: View {
     @Previewable var animateAvatarForQuickname: Bool = false
     @Previewable @FocusState var focusState: MessagesViewInputFocus?
     @Previewable @State var viewModelFocus: MessagesViewInputFocus?
-    MessagesBottomBar(
-        profile: profile,
-        displayName: $profileName,
-        messageText: $messageText,
-        sendButtonEnabled: $sendButtonEnabled,
-        profileImage: $profileImage,
-        focusState: $focusState,
-        viewModelFocus: viewModelFocus,
-        animateAvatarForQuickname: animateAvatarForQuickname,
-        messagesTextFieldEnabled: true,
-        onProfilePhotoTap: {
-            viewModelFocus = .displayName
-        },
-        onSendMessage: {},
-        onDisplayNameEndedEditing: {
-            viewModelFocus = .message
-        },
-        onProfileSettings: {}
-    )
+    @Previewable @State var bottomBarHeight: CGFloat = 0.0
+
+    Group {
+        MessagesViewRepresentable(
+            conversation: .mock(),
+            messages: [],
+            invite: .mock(),
+            onTapMessage: { _ in },
+            onTapAvatar: { _ in },
+            bottomBarHeight: bottomBarHeight
+        )
+        .ignoresSafeArea()
+    }
+    .safeAreaBar(edge: .bottom) {
+        VStack(spacing: 0.0) {
+            MessagesBottomBar(
+                profile: profile,
+                displayName: $profileName,
+                messageText: $messageText,
+                sendButtonEnabled: $sendButtonEnabled,
+                profileImage: $profileImage,
+                focusState: $focusState,
+                viewModelFocus: viewModelFocus,
+                animateAvatarForQuickname: animateAvatarForQuickname,
+                messagesTextFieldEnabled: true,
+                onProfilePhotoTap: {
+                    viewModelFocus = .displayName
+                },
+                onSendMessage: {},
+                onDisplayNameEndedEditing: {
+                    viewModelFocus = .message
+                },
+                onProfileSettings: {}
+            )
+            .background(HeightReader())
+            .onPreferenceChange(HeightPreferenceKey.self) { height in
+                bottomBarHeight = height
+            }
+        }
+        .background(HeightReader())
+        .onPreferenceChange(HeightPreferenceKey.self) { height in
+            bottomBarHeight = height
+        }
+    }
 }
